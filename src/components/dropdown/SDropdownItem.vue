dd451f16f0fd08c74f8f659a1757ce89e1ceb912
sonncho <dungcho93@gmail.com>
2022-11-30 18:00:49 +0900

    fix : invalid vnode type when creating vnode dropdown item 내 icon 컴포넌트를 불러오는 곳에 'invalid vnode type when creating vnode .' [vue warn]발생 computed속성으로 감싸줌.

--- src/components/dropdown/SDropdownItem.vue
+++ src/components/dropdown/SDropdownItem.vue
@@ -1,14 +1,27 @@
 <template lang="">
   <li class="dropdown-item">
     <router-link :to="to" class="nav-link">
-
+      <component :is="iconComponent" class="h-4 inline mr-2" />
       <slot></slot>
     </router-link>
   </li>
 </template>
 <script>
+import { computed } from 'vue';
+import {
+  RectangleStackIcon,
+  QueueListIcon,
+  PaintBrushIcon,
+  MusicalNoteIcon,
+} from '@heroicons/vue/24/outline';
 export default {
   name: 'SDropdownItem',
+  components: {
+    RectangleStackIcon,
+    QueueListIcon,
+    PaintBrushIcon,
+    MusicalNoteIcon,
+  },
   props: {
     to: {
       type: String,
@@ -18,6 +31,12 @@ export default {
       type: String,
       default: '',
     },
+  },
+  setup(props) {
+    const iconComponent = computed(() => {
+      return props.icon || null;
+    });
+    return { iconComponent };
   },
 };
 </script>
